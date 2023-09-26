a559c5fac5a9242da5bb07021c519733926f7ab8
faithnekesa <faynekesa@gmail.com>
2023-09-26 12:32:37 -0700

    Adding task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""define the class, Square"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Class Square that defines a square by: (based on 0-square.py)
+        """
+        self.__size = size
