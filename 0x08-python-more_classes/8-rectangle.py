f8badc95a366b0178ce9f87b9e99fc25dc41bc13
faithnekesa <faynekesa@gmail.com>
2023-10-02 09:43:53 -0700

    Adding rectange task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -77,3 +77,20 @@ class Rectangle:
         """Print message for every instance deletion of the reactangle"""
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Return the Rectangle with the greater area
+        Args:
+            rect_1 (Rectangle object): First Rectangle
+            rect_2 (Rectangle object): Second Rectangle
+        Raises:
+            TypeError:If rect_1 or rect_2 is not a rectangle instance
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return (rect_1)
+        return (rect_2)
