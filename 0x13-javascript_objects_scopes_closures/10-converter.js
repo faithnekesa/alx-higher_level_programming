6f532cbf53279bbc60ff50b7b6ed8ec97c8e0235
faithnekesa <faynekesa@gmail.com>
2023-12-14 03:51:05 -0800

    Adding advanced task 1

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // function that converts a number from base 10 to another
-//base passed as argument
+// base passed as argument
 
 exports.converter = function (base) {
   return function (n) {
