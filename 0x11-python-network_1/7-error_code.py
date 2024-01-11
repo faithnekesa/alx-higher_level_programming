7c7cb3f491d5776db9ac7534330a2a6c61ec1642
faithnekesa <faynekesa@gmail.com>
2024-01-11 06:05:15 -0800

    Adding python network1 task 3-8

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and displays
+the body of the response. If the HTTP status code is greater than or equal to
+400, print: Error code: followed by the value of the HTTP status code
+"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    r = requests.get(argv[1])
+    status = r.status_code
+    print(r.text) if status < 400 else print(
+        "Error code: {}".format(r.status_code))
